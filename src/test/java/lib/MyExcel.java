8b6bcdf12073cb186a1b0c2136400108f46a5061
yes4me <yes4me@hotmail.com>
2015-08-16 05:22:12 -0700

    Add download link in comment

--- src/test/java/lib/MyExcel.java
+++ src/test/java/lib/MyExcel.java
@@ -2,7 +2,7 @@
 Created:	2015/08/15 - https://github.com/yes4me/
 Author:		Thomas Nguyen - thomas_ejob@hotmail.com
 Purpose:	My library for Excel files
-Requires: FROM: Apache POI
+Requires: FROM: Apache POI - https://poi.apache.org/download.html
 	poi-3.12-20150511.jar
 	poi-ooxml-3.12-20150511.jar
 	poi-ooxml-schemas-3.12-20150511.jar
@@ -18,6 +18,7 @@ import java.io.IOException;
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.WorkbookUtil;
 
 public class MyExcel {
 	public boolean readExcel(String file) {
